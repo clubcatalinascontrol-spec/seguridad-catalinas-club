d2e999f4fcdaedf6da3d3fe0b472623885ce7c85
clubcatalinascontrol-spec <clubcatalinas.control@gmail.com>
2025-09-20 09:01:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-if(!localStorage.getItem("adminPass")) localStorage.setItem("adminPass","123456789");
+if(!localStorage.getItem("adminPass")) localStorage.setItem("adminPass","1234"); // por defecto
 
 const navBtns = document.querySelectorAll(".nav-btn");
 const pages = document.querySelectorAll(".page");
